875e303224e50913d84d7f6a5716426533d40bf5
Jakub <jakub.pawel.krol@gmail.com>
2016-01-12 14:45:11 +0100

    ble

---
+++ src/main/java/HelloMaven/com/hello/tutorial/App.java
@@ -0,0 +1,17 @@
+package HelloMaven.com.hello.tutorial;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+	public static void main(String[] args) {
+	      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+	      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	      HelloWorld obj2 = (HelloWorld) context.getBean("probnaMetoda");
+
+	      obj.getMessage();
+	      obj2.getMessage2();
+	   }
+}
